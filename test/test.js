42d6c27a904508a036a1b96a6498921a20be98bc
Phooyau-W <825495382@qq.com>
2020-09-28 18:27:14 +0800

    Travis-CI 线上构建测试

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ Vue.config.productionTip = false
 
 describe('我的 Vue 测试', function () {
   describe('#标题', function () {
-    it('标题应该为 Welcome to Your Vue.js App', function () {
+    it('页面内容符合预期', function () {
       // 示例化 Vue, 此时示例对应的 DOM 元素没有在页面中。
       const instance = new Vue({
         render: h => h(App),
@@ -16,7 +16,9 @@ describe('我的 Vue 测试', function () {
       document.body.appendChild(instance.$el)
 
       let h1 = instance.$el.querySelector('.hello h1')
+      let h2 = instance.$el.querySelector('.hello h2')
       expect(h1.textContent).to.equal('Welcome to Your Vue.js App')
+      expect(h2.innerText).to.equal('Essential Links')
     })
   })
 })
